1070441d8343507dca41fb1198da9750cc1c3ddf
Kalpesh Laddha <laddha.kalpesh@gmail.com>
2026-03-14 12:56:57 -0400

    Added the structure of the project

---
+++ payment-processing-service/src/main/java/com/payment/processing/service/PaymentProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.payment.processing.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentProcessingServiceApplication.class, args);
+	}
+
+}
